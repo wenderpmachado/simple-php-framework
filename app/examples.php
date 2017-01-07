779b37e2931f6e624fd1cb538a5557abcad04ee3
wenderpmachado <wender_machado@msn.com>
2017-01-07 17:42:37 -0200

    update composer.json

--- app/examples.php
+++ app/examples.php
@@ -44,10 +44,3 @@ $addressRepository = DI::create('AddressRepository');
 $address = $addressRepository->select(1);
 var_dump($address);
 var_dump($addressRepository->user($address));
-
-
-
-
-
-
-//127.0.0.1:46841 [500]: / - Uncaught PDOException: SQLSTATE[42000]: Syntax error or access violation: 1064 You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '.* FROM 'user' JOIN 'address' ON 'address'.'user_id' = 'user'.id WHERE 'address'' at line 1 in /var/www/html/0_open-source/simples-php-framework/vendor/phputil/pdowrapper/lib/PDOWrapper.php:300
